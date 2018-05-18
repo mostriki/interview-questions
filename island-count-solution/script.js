e1b30cdaae92b3a14a92859f8d730e0025167bed
Riki Montgomery <mostriki820@gmail.com>
2018-05-18 08:38:57 -0700

    Add visited array to check against what nodes it's been to.

--- island-count-solution/script.js
+++ island-count-solution/script.js
@@ -13,31 +13,38 @@ const graph = [
 
 function islandFinder(matrix) {
   let total = 0;
+  let visited = [];
+
+  for(var i = 0; i < matrix.length; i++) {
+    visited[i] = new Array(matrix[i].length).map(function(i) { return false; });
+  }
 
   for(let row = 0; row < matrix.length; row++) {
     for(let col = 0; col < matrix[row].length; col++){
-      if(matrix[row][col] == 1){
+      if(matrix[row][col] == 1 && !visited[row][col]){
         total ++;
-        searchForLand(matrix, row, col);
+        searchForLand(matrix, row, col, visited);
       }
     }
   }
   return total;
 }
 
-function searchForLand(matrix, row, col) {
+function searchForLand(matrix, row, col, visited) {
   let rowPlus1  = row + 1;
   let columnPlus1  = col + 1
   let columnMinus1 = col - 1;
+  visited[row][col] = true;
 
   if (matrix[row][col] == 1) {
       matrix[row][col] = 0;                     
-      searchForLand(matrix, rowPlus1, col);       // Recurse South
-      searchForLand(matrix, row, columnPlus1);       // Recurse East
-      searchForLand(matrix, rowPlus1, columnPlus1);  // Recures diagonal Southeast
-      searchForLand(matrix, rowPlus1, columnMinus1); // Recures diagonal Southwest
+      searchForLand(matrix, rowPlus1, col, visited);          // Recurse South
+      searchForLand(matrix, row, columnPlus1, visited);       // Recurse East
+      searchForLand(matrix, rowPlus1, columnPlus1, visited);  // Recures diagonal Southeast
+      searchForLand(matrix, rowPlus1, columnMinus1, visited); // Recures diagonal Southwest
   }
 }
 
 console.log(islandFinder(graph));
+
 
